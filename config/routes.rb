3587bfba8b2806a0f32f7fbe6aff731103b9a003
Roel Castaño <roelcastanomoreno@gmail.com>
2015-12-28 12:36:16 -0600

    Added tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 #Rails.application.routes.draw do
 
+require 'api_constraints'
 MarketPlaceApi::Application.routes.draw do
+
   # API definition
   namespace :api, defaults: {format: :json },
    constraints: {subdomain: 'api'}, path: '/' do
-    # List resources here
+
+    scope module: :v1,
+      constraints: ApiConstraints.new(version: 1, default: true) do
+
+    end
   end
 
 end
